35110946849e598051a65c30b29f1ea2e6b3a886
broothie <adbooth8@gmail.com>
2023-08-01 22:31:18 -0700

    add doc comments

--- case.go
+++ case.go
@@ -5,18 +5,24 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// CaseFunc is the signature of functions passed in (typically anonymously) to *Context.It, *Context.BeforeEach, and
+// *Context.AfterEach.
 type CaseFunc func(c *Case)
 
+// Case provides a handle for test cases to make assertions via *Case.Assert.
+// It also provides *Case.Require for assertions that immediately fail the test case.
 type Case struct {
 	context  *Context
 	testingT testingT
 	lets     map[string]any
 }
 
+// Assert provides a reference to a test case's *assert.Assertions.
 func (c *Case) Assert() *assert.Assertions {
 	return assert.New(c.testingT)
 }
 
+// Require provides a reference to a test case's *require.Assertions.
 func (c *Case) Require() *require.Assertions {
 	return require.New(c.testingT)
 }
--- context.go
+++ context.go
@@ -4,8 +4,11 @@ import (
 	"strings"
 )
 
+// ContextFunc is the signature of functions passed in (typically anonymously) to gspec.Run, gspec.Describe,
+// *Context.Describe, and *Context.Context.
 type ContextFunc func(c *Context)
 
+// Context provides a handle for test groups to define test cases, nested groups, lets, and hooks.
 type Context struct {
 	parent *Context
 	name   string
@@ -22,10 +25,13 @@ type caseEntry struct {
 	run  CaseFunc
 }
 
+// Describe defines a nested group labelled with the provided subject.
 func (c *Context) Describe(subject string, f ContextFunc) {
 	c.Context(subject, f)
 }
 
+// Context defines a nested group labelled with the provided context.
+// Context labels typically begin with "when", "with", or "without".
 func (c *Context) Context(context string, f ContextFunc) {
 	ctx := &Context{parent: c, name: context, lets: make(map[string]letFunc)}
 	c.contexts = append(c.contexts, ctx)
@@ -33,6 +39,7 @@ func (c *Context) Context(context string, f ContextFunc) {
 	f(ctx)
 }
 
+// It defines a test case labelled with the provided behavior.
 func (c *Context) It(behavior string, f CaseFunc) {
 	c.cases = append(c.cases, caseEntry{
 		name: behavior,
